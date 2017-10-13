93479a9206c4831c533b5a717dc0335047b7253f
Arjunxxxl <a.asankhala@gmail.com>
2017-10-13 22:39:56 +0530

    Add files via upload

---
+++ Player/PlayerMovement.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float speed = 6f;
+
+	Vector3 movement;
+	Animator anim;
+	Rigidbody playerRigidbody;
+	int floorMast;
+	float camRayLength=100;
+
+	void Awake()
+	{
+		floorMast = LayerMask.GetMask ("Floor");
+		anim = GetComponent<Animator> ();
+		playerRigidbody = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate()
+	{
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+		// Move the player around the scene.
+		Move (h, v);
+
+		// Turn the player to face the mouse cursor.
+		Turning ();
+
+		// Animate the player.
+		Animating (h, v);
+
+	}
+
+	void Move(float h,float v)
+	{
+		movement.Set (h, 0f, v);
+
+		movement = movement.normalized * speed * Time.deltaTime;
+		playerRigidbody.MovePosition (transform.position + movement);
+	}
+
+	void Turning()
+	{
+		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit floorHit;
+		if (Physics.Raycast (camRay, out floorHit, camRayLength, floorMast))
+			{
+			Vector3 playerToMouse = floorHit.point - transform.position;
+			playerToMouse.y = 0f;
+
+			Quaternion newRotation = Quaternion.LookRotation (playerToMouse);
+			playerRigidbody.MoveRotation (newRotation);
+			}
+	}
+
+	void Animating (float h, float v)
+	{
+		// Create a boolean that is true if either of the input axes is non-zero.
+		bool walking = h != 0f || v != 0f;
+
+		// Tell the animator whether or not the player is walking.
+		anim.SetBool ("IsWalking", walking);
+	}
+}
